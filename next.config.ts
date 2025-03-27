81de75400c64155a92297b4515a0721bfaba006b
Rayan Kazi <rayankazi7515@gmail.com>
2025-03-26 19:56:45 -0700

    added a bunch of config things

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createMdx from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
-export default nextConfig;
+const withMdx = createMdx({});
+
+export default withMdx(nextConfig);
