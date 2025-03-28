714371670d20a3a23713ab73cbd8619b2d06b4e6
Rayan Kazi <rayankazi7515@gmail.com>
2025-03-27 20:26:07 -0700

    made changes to the home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+import { Avatar } from "@/components/ui/avatar";
+import { Link } from "next-view-transitions";
 
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <main className="flex flex-col gap-16 md:gap-24">
+      <div className="flex flex-col gap-4">
+        <div className="block md:hidden">
+          <Avatar width={56} height={56} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="font-medium">Hello there 👋</span>
+
+          <p className="text-low-contrast-text">
+            I&apos;m a full stack engineer who&apos;s passionate about building
+            amazing software.
+          </p>
+
+          <p className="text-low-contrast-text">
+            I often write about my experiences on my{" "}
+            <Link
+              className="transition duration-200 hover:opacity-90 text-high-contrast-text"
+              href="/blog"
+            >
+              blog
+            </Link>
+            .
+          </p>
+        </div>
+
+        <div>hi</div>
+      </div>
+    </main>
+  );
 }
