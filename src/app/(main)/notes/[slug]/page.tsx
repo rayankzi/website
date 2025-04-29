92b20743ada0749713722837aa74febb5334b1d0
Rayan Kazi <rayankazi7515@gmail.com>
2025-04-28 17:34:58 -0700

    fixed dynamic metadata for projects, notes, and blog

--- src/app/(main)/notes/[slug]/page.tsx
+++ src/app/(main)/notes/[slug]/page.tsx
@@ -1,4 +1,23 @@
+import { siteMetadata } from "@/lib/data";
 import { redirect } from "next/navigation";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  try {
+    const { metadata } = await import(`@/content/notes/${slug}.mdx`);
+    console.log(metadata);
+
+    return { title: metadata.title };
+  } catch (error) {
+    console.error("Error occured", error);
+    return { title: siteMetadata.title };
+  }
+}
 
 export default async function NoteItemPage({
   params,
